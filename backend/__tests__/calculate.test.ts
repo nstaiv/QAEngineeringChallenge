8cf9e770970774e3b13d98c92d5117d9c2b2158f
nstaiv <130700356+nstaiv@users.noreply.github.com>
2023-09-18 22:35:03 -0700

    (Jest) Add calculation tests.

--- backend/__tests__/calculate.test.ts
+++ backend/__tests__/calculate.test.ts
@@ -2,9 +2,14 @@ import {calculatePartHealth, calculateMachineHealth} from '../calculations';
 import {
   MachineType,
   WeldingRobotPart,
+  PaintingStationPart,
+  AssemblyLinePart,
+  QualityControlStationPart,
   partInfo,
 } from '../../native-app/data/types';
 
+
+// Welding Robot machine calculations
 describe('calculatePartHealth', () => {
   it('calculates part health correctly', () => {
     const machineName: MachineType = MachineType.WeldingRobot;
@@ -30,6 +35,166 @@ describe('calculateMachineHealth', () => {
       {name: WeldingRobotPart.CoolingEfficiency, value: 85.0},
     ];
     const expectedHealth = 76.70138888888889;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+// Painting Station machine calculations
+describe('calculate Painting Station Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.PaintingStation;
+    const part: partInfo = {name: PaintingStationPart.FlowRate, value: 15.0};
+    const expectedHealth = 100.00;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Painting Station Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.PaintingStation;
+    const parts = [
+      {name: PaintingStationPart.Pressure, value: 55.0},
+      {name: PaintingStationPart.NozzleCondition, value: 0.5},
+    ];
+    const expectedHealth = 75.00;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+// Assembly Line machine calculations
+describe('calculate Assembly Line Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.AssemblyLine;
+    const part: partInfo = {name: AssemblyLinePart.AlignmentAccuracy, value: 0.5};
+    const expectedHealth = 72.22222222222223;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Assembly Line Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.AssemblyLine;
+    const parts = [
+      {name: AssemblyLinePart.AlignmentAccuracy, value: 0.5},
+      {name: AssemblyLinePart.BeltSpeed, value: 1.5},
+      {name: AssemblyLinePart.FittingTolerance, value: 0.02},
+      {name: AssemblyLinePart.Speed, value: 8.0},
+    ];
+    const expectedHealth = 72.43055555555556;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+// Quality Control Station machine calculations
+describe('calculate Quality Control Station Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.QualityControlStation;
+    const part: partInfo = {name: QualityControlStationPart.CameraCalibration, value: 0.7};
+    const expectedHealth = 85;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Quality Control Station Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.QualityControlStation;
+    const parts = [
+      {name: QualityControlStationPart.CameraCalibration, value: 0.7},
+      {name: QualityControlStationPart.CriteriaSettings, value: 0.8},
+      {name: QualityControlStationPart.LightIntensity, value: 93.0},
+    ];
+    const expectedHealth = 85;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});// Painting Station machine calculations
+describe('calculate Painting Station Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.PaintingStation;
+    const part: partInfo = {name: PaintingStationPart.FlowRate, value: 15.0};
+    const expectedHealth = 100.00;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Painting Station Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.PaintingStation;
+    const parts = [
+      {name: PaintingStationPart.Pressure, value: 55.0},
+      {name: PaintingStationPart.NozzleCondition, value: 0.5},
+    ];
+    const expectedHealth = 75.00;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+// Assembly Line machine calculations
+describe('calculate Assembly Line Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.AssemblyLine;
+    const part: partInfo = {name: AssemblyLinePart.AlignmentAccuracy, value: 0.5};
+    const expectedHealth = 72.22222222222223;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Assembly Line Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.AssemblyLine;
+    const parts = [
+      {name: AssemblyLinePart.AlignmentAccuracy, value: 0.5},
+      {name: AssemblyLinePart.BeltSpeed, value: 1.5},
+      {name: AssemblyLinePart.FittingTolerance, value: 0.02},
+      {name: AssemblyLinePart.Speed, value: 8.0},
+    ];
+    const expectedHealth = 72.43055555555556;
+
+    const result = calculateMachineHealth(machineName, parts);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+// Quality Control Station machine calculations
+describe('calculate Quality Control Station Part Health', () => {
+  it('calculates part health correctly', () => {
+    const machineName: MachineType = MachineType.QualityControlStation;
+    const part: partInfo = {name: QualityControlStationPart.CameraCalibration, value: 0.7};
+    const expectedHealth = 85;
+
+    const result = calculatePartHealth(machineName, part);
+    expect(result).toBe(expectedHealth);
+  });
+});
+
+describe('calculate Quality Control Station Machine Health', () => {
+  it('calculates machine health correctly', () => {
+    const machineName: MachineType = MachineType.QualityControlStation;
+    const parts = [
+      {name: QualityControlStationPart.CameraCalibration, value: 0.7},
+      {name: QualityControlStationPart.CriteriaSettings, value: 0.8},
+      {name: QualityControlStationPart.LightIntensity, value: 93.0},
+    ];
+    const expectedHealth = 85;
 
     const result = calculateMachineHealth(machineName, parts);
     expect(result).toBe(expectedHealth);
